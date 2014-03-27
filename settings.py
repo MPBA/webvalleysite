934dd9cb023c35decb13416f6504074c2be31f59
Luca Coviello <luca.coviello@gmail.com>
2014-03-27 21:13:01 +0100

    Added fbk db to the settings

--- settings.py
+++ settings.py
@@ -164,7 +164,22 @@ FILE_UPLOAD_PERMISSIONS = 0o644
 # DATABASES #
 #############
 
-DATABASES = None
+DATABASES = {
+    "default": {
+        # Add "postgresql_psycopg2", "mysql", "sqlite3" or "oracle".
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        # DB name or path to database file if using sqlite3.
+        "NAME": "wvdevel",
+        # Not used with sqlite3.
+        "USER": "webvalley",
+        # Not used with sqlite3.
+        "PASSWORD": "Webv!!usr",
+        # Set to empty string for localhost. Not used with sqlite3.
+        "HOST": "geopg.fbk.eu",
+        # Set to empty string for default. Not used with sqlite3.
+        "PORT": "5432",
+    }
+}
 
 
 #########
