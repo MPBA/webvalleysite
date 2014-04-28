dca4d1b0947b8604a9ea4702f97decd97ca987fa
ernesto <ernesto.arbitrio@gmail.com>
2014-04-28 11:39:08 +0200

    download profiles

--- accounts/views.py
+++ accounts/views.py
@@ -48,12 +48,13 @@ def browse_applications(request, url):
     path = settings.PROJECT_ROOT
     myfiles = os.path.join(path, 'static/media/')
     mylist = os.path.join(myfiles, url)
+    print mylist
     os.chdir(mylist)
     files = os.listdir(".")
     file_dict = []
     for f in files:
         if os.path.isdir(f):
-            file_dict.append({'name': f, 'link': os.path.join(mylist, f), 'type': 'directory'})
+            file_dict.append({'name': f, 'link': os.path.join('/static/media', url, f), 'type': 'directory'})
         else:
             file_dict.append({'name': f, 'link': os.path.join('/static/media', url, f), 'type': 'file'})
     context = {'filelist': file_dict}
