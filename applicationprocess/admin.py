54596d04cc3481e1a9ab05b14a99b6a3c52f3935
Luca Coviello <luca.coviello@gmail.com>
2014-05-06 18:26:37 +0200

    search and filter user form

--- applicationprocess/admin.py
+++ applicationprocess/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from models import Form,ApplicationProcess,Validator,\
-    Field,FieldType,ValidatorType,ApplicationStatus,ApplicationProcessForm,UserForm
+    Field, FieldType, ValidatorType, ApplicationStatus, ApplicationProcessForm, UserForm
 
 class ApplicationStatusAdmin(admin.ModelAdmin):
     search_fields = ('user_profile__user__first_name', 'user_profile__user__last_name')
     list_filter = ('application_process', 'status')
+
+class UserFormAdmin(admin.ModelAdmin):
+    search_fields = ('application_status__user_profile__user__first_name', 'application_status__user_profile__user__last_name')
+    list_filter = ('form',)
 
 admin.site.register(Form)
 admin.site.register(ApplicationProcess)
@@ -14,4 +18,4 @@ admin.site.register(ValidatorType)
 admin.site.register(Field)
 admin.site.register(FieldType)
 admin.site.register(ApplicationProcessForm)
-admin.site.register(UserForm)
+admin.site.register(UserForm, UserFormAdmin)
