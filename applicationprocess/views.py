7a55ae524c79ef493db623a9e040a3d0923fec6b
Luca Coviello <luca.coviello@gmail.com>
2014-05-08 09:43:14 +0200

    update status at last

--- applicationprocess/views.py
+++ applicationprocess/views.py
@@ -190,11 +190,11 @@ def submit(request):
     elif request.method == 'POST':
         profile = request.user.get_profile()
         if profile.applicationstatus.ready_for_submission():
-            a_s = profile.applicationstatus
-            a_s.status = 'S_SUB'
-            a_s.save()
             do_submission( profile )
             do_final_submission( profile )
+            a_s = profile.applicationstatus
+            a_s.staftus = 'S_SUB'
+            a_s.save()
             return render(request, 'application_process/submit_done.html')
         else:
             return render(request, 'application_process/submit_confirm.html')
