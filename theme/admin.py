35f33498cbcc67ce9b10b2c766c72f301a8e0727
Luca Coviello <luca.coviello@gmail.com>
2015-04-20 16:11:21 +0200

    Added date_joined in user admin

--- theme/admin.py
+++ theme/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("date_joined", )
+
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
