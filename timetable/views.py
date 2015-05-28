69f511e3067e4bc9db207cc552c07827f184ef88
Luca Coviello <luca.coviello@gmail.com>
2015-05-28 14:03:53 +0200

    damn google, u got me again

--- timetable/views.py
+++ timetable/views.py
@@ -1,9 +1,14 @@
 import csv
+import logging
 
+from django.http import HttpResponseRedirect
+from django.shortcuts import render_to_response
+import httplib2
 from django.conf import settings
 from django.shortcuts import render
 from django.core.urlresolvers import reverse
 from django.core.cache import cache
+
 try:
     from cStringIO import StringIO
 except ImportError:
@@ -12,47 +17,67 @@ except ImportError:
 import gspreadsheet as gss
 
 from mezzanine.pages.models import Page
-from .models import Timetable
+
+import os
+import logging
+import httplib2
+
+from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse
+from django.http import HttpResponse
+from django.http import HttpResponseBadRequest
+from django.http import HttpResponseRedirect
+from django.shortcuts import render_to_response
+
 
 def display(request):
     page = Page.objects.get(slug=request.META['PATH_INFO'][1:-1])
-    print page.title
     raw_data = csv.reader(StringIO(_csv_get(page)))
     data = []
     for row in raw_data:
         if row[1]:
-            data.append({ 'date': row[0]+u', '+row[1],
-                          'activities': [] })
-        act = { 'time': row[2],
-                'name': row[3],
-                'teachers': row[4],
-                'links': [] }
+            data.append({
+                'date': row[0]+u', '+row[1],
+                'activities': []
+            })
+
+        act = {
+            'time': row[2],
+            'name': row[3],
+            'teachers': row[4],
+            'links': []
+        }
         for cell in row[5:]:
             if cell:
                 if 'dropbox' in cell:
                     ext = cell.split('.')[-1]
                 else:
                     ext = "WWW"
-                act['links'].append({ 'href': cell,
-                                      'extension': ext })
+                act['links'].append({
+                    'href': cell,
+                    'extension': ext
+                })
         data[-1]['activities'].append(act)
-
     return render(request, "timetable/display.html", {'data': data,
                                                       'page_title': 'Time table',
                                                       'sidebar_item': 'timetable',})
+
 
 def _csv_get(page):
     """
     Return the csv file as a string, downloading it if needed.
     """
     cache_key = reverse('timetable.views.display')
+
     ret = cache.get(cache_key)
     if ret is not None:
         return ret
     else:
         ret = _csv_download(page)
         cache.set(cache_key, ret, timeout=15)  # cache lasts 15 seconds
+
         return ret
+
 
 def _csv_download(page):
     """
