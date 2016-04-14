eedccb1a9f9c97a798b6bf60bc11616c886771b2
Luca Coviello <luca.coviello@gmail.com>
2016-04-14 15:19:22 +0200

    updated max_length for email in signup template

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,12 +3,15 @@ from django.utils.translation import ugettext_lazy as _
 from django.core import validators
 from django.contrib.auth.models import User
 
+
 class RegistrationForm(forms.Form):
     first_name = forms.CharField(max_length=30, required=True, label=_('First name'))
     last_name = forms.CharField(max_length=30, required=True, label=_('Last name'))
-    mail = forms.EmailField(max_length=30, required=True,label=_('E-mail'))
-    password1 = forms.CharField(max_length=30, required=True,widget=forms.PasswordInput(render_value=False), label=_('Password'))
-    password2 = forms.CharField(max_length=30, required=True,widget=forms.PasswordInput(render_value=False), label=_('Confirm password'))
+    mail = forms.EmailField(max_length=40, required=True, label=_('E-mail'))
+    password1 = forms.CharField(max_length=30, required=True, widget=forms.PasswordInput(render_value=False),
+                                label=_('Password'))
+    password2 = forms.CharField(max_length=30, required=True, widget=forms.PasswordInput(render_value=False),
+                                label=_('Confirm password'))
 
     # def __init__(self, *args, **kwargs):
     #     super(RegistrationForm, self).__init__(*args, **kwargs)
@@ -43,5 +46,3 @@ class RegistrationForm(forms.Form):
         except User.DoesNotExist:
             return form_email
         raise validators.ValidationError('Email "{email}" is already taken.'.format(email=form_email))
-
-
