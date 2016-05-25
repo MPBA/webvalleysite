dab324f1b2173fc2df6ff02fb3fc5ff65b82f1c7
Luca Coviello <luca.coviello@gmail.com>
2016-05-25 16:43:00 +0200

    managing order for categories

--- team/templatetags/customtags.py
+++ team/templatetags/customtags.py
@@ -7,9 +7,11 @@ register = Library()
 @register.filter
 def categories_dict(components):
     categories = {}
+    orders = {}
     for i in components:
         if i.category.category not in categories:
             categories[i.category.category] = []
+            orders[i.category.order] = i.category.category
         categories[i.category.category].append(i)
 
-    return categories.items()
+    return [(v, categories[v]) for k, v in orders.iteritems()]
