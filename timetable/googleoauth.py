ed94961997442ac766d1b713ffcd740340e39ed7
Illedran <nrd.nardelli@gmail.com>
2017-01-31 12:23:52 +0100

    Fixed OAuth for timetable generation

--- timetable/googleoauth.py
+++ timetable/googleoauth.py
@@ -1,23 +1,14 @@
 import requests, gspread, os, ast
-from oauth2client.client import SignedJwtAssertionCredentials
-
+from oauth2client.service_account import ServiceAccountCredentials
+from httplib2 import Http
 
 def authenticate_google_docs():
-    f = file(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'WebValley2014-72837d7af4fc.p12'), 'rb')
-    SIGNED_KEY = f.read()
-    f.close()
+
+    path_to_json_key = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'WebValley2014-3f9dbe414b8a.json')
     scope = ['https://spreadsheets.google.com/feeds', 'https://docs.google.com/feeds']
-    credentials = SignedJwtAssertionCredentials('webvalleyfbk@gmail.com', SIGNED_KEY, scope)
 
-    data = {
-        'refresh_token' : '1/G6NjKS1sMff-sIPblMCM3nrw3Hu41tuCfKIaFs1cL_bBactUREZofsF9C7PrpE-j',
-        'client_id' : '68799219595-oguekfb6ejolblop6je8gjkhh7q4b7re.apps.googleusercontent.com',
-        'client_secret' : 'XRM3Z8yL0tkpNACBrQmxZjAd',
-        'grant_type' : 'refresh_token',
-    }
-
-    r = requests.post('https://accounts.google.com/o/oauth2/token', data = data)
-    credentials.access_token = ast.literal_eval(r.text)['access_token']
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(path_to_json_key, scopes=scope)
 
     gc = gspread.authorize(credentials)
     return gc
+
